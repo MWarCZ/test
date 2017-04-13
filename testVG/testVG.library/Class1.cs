e6085fbb53f212ea5a61d26bf34c4833fab08004
MWarCZ <xvalka05@stud.fit.vutbr.cz>
2017-04-13 07:51:59 +0200

    Zalozeni projektu v VS a zalozeni knihovny a testu ke knihovne.

---
+++ testVG/testVG.library/Class1.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace testVG.library
+{
+    public class GiveMeValue
+    {
+        private int value;
+        public GiveMeValue()
+        {
+
+        }
+
+        public int GetValue()
+        {
+            return this.value;
+        }
+        public void SetValue(int value)
+        {
+            this.value = value;
+        }
+    }
+}
---
+++ testVG/testVG.unit_test/UnitTest1.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using testVG.library;
+
+namespace testVG.unit_test
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            GiveMeValue gmv = new GiveMeValue();
+        }
+    }
+}
